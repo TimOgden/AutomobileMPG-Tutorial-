15e94f47ea4f06720a6d21f4bd82a381811e2e2b
Tim Ogden <togden1999@gmail.com>
2018-12-16 18:27:26 -0500

    Training the model and graphing the errors over 1000 epochs

--- regressionProblem.py
+++ regressionProblem.py
@@ -39,7 +39,7 @@ plt.show()
 train_stats = train_dataset.describe()
 train_stats.pop("MPG") #Pop removes the category or index we give, and we need to remove MPG because thats what the regression needs to predict
 train_stats = train_stats.transpose()
-print(train_stats)
+#print(train_stats)
 
 train_labels = train_dataset.pop("MPG")
 test_labels = test_dataset.pop("MPG") #Not sure what these two lines do. Wouldn't the labels and the data be separate entirely?
@@ -48,3 +48,55 @@ def norm(x):
 	return (x-train_stats['mean'])/train_stats['std']
 normed_train_data = norm(train_dataset)
 normed_test_data = norm(test_dataset)
+
+def build_model(): #Not sure why you would put this block in a function. Maybe if you were gonna have multiple networks?
+	model = keras.Sequential([
+		layers.Dense(64, activation=tf.nn.relu, input_shape=[len(train_dataset.keys())]),
+		layers.Dense(64, activation=tf.nn.relu),
+		layers.Dense(1)
+	])
+	
+	optimizer = tf.train.RMSPropOptimizer(0.001)
+	model.compile(loss='mse', optimizer=optimizer, metrics=['mae','mse'])
+	
+	return model
+
+model = build_model()
+#print(model.summary())
+
+example_batch = normed_train_data[:10]
+example_result = model.predict(example_batch)
+print(model.predict(example_batch)) #Testing a batch of inputs on the untrained model to make sure it outputs the right shape.
+
+class PrintDot(keras.callbacks.Callback):
+	def on_epoch_end(self, epoch, logs):
+		if(epoch%100==0): print('')
+		print('.', end=' ')
+
+history = model.fit(normed_train_data, train_labels, epochs=1000, validation_split=.2, verbose=0, callbacks=[PrintDot()])
+
+hist = pd.DataFrame(history.history)
+hist['epoch'] = history.epoch
+print(hist.tail())
+
+#Let's make a function to graph the trends of the error
+def plot_history(history):
+	plt.figure()
+	plt.xlabel('Epoch')
+	plt.ylabel('Mean Abs Error [MPG]')
+	plt.plot(hist['epoch'], hist['mean_absolute_error'], label=['Train Error'])
+	plt.plot(hist['epoch'], hist['val_mean_absolute_error'], label=['Val Error'])
+	plt.legend()
+	plt.ylim([0,5])
+	plt.show()
+	
+	plt.figure()
+	plt.xlabel('Epoch')
+	plt.ylabel('Mean Square Error [$MPG^2$]')
+	plt.plot(hist['epoch'], hist['mean_squared_error'], label=['Train Error'])
+	plt.plot(hist['epoch'], hist['val_mean_squared_error'], label=['Val Error'])
+	plt.ylim(0,20)
+	plt.legend()
+	plt.show()
+
+plot_history(history)
